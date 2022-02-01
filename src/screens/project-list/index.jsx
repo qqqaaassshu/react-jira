42ec724d3ea6844cf4d7a82154719cfd0e6e386b
qqqaaassshu <1845348325@qq.com>
2022-02-01 14:43:06 +0800

    feat: 新增useDebounce方法

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,43 +2,47 @@
  * @Author: 胡纯明
  * @LastEditors: 胡纯明
  * @Date: 2022-01-31 16:10:04
- * @LastEditTime: 2022-01-31 18:31:40
+ * @LastEditTime: 2022-01-31 22:57:34
  * @Description: 列表入口文件
  * @FilePath: /jira/src/screens/project-list/index.jsx
  */
 import { useState, useEffect } from "react";
 import SearchPanel from "./SearchPanel";
 import List from "./List";
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const ProjectList = () => {
   const [users, setUsers] = useState([]);
+
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
+
+  const debouncedParam = useDebounce(param, 500);
+
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@
  * @Author: 胡纯明
  * @LastEditors: 胡纯明
  * @Date: 2022-01-31 18:26:06
- * @LastEditTime: 2022-01-31 18:30:46
+ * @LastEditTime: 2022-01-31 22:57:48
  * @Description: 工具方法集
  * @FilePath: /jira/src/utils/index.js
  */
+
+import { useEffect, useState } from "react";
 
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
@@ -18,4 +20,21 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
